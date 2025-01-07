b7543b1375bc75ac3ac49295c7236457da5dd14d
AlyNefel <ali.essoudani@nefel.education>
2025-01-07 13:25:16 +0100

    Week 1/Day2

---
+++ Week1/Day2/TestNinja.java
@@ -0,0 +1,30 @@
+public class TestNinja{
+    public static void main(String[] args) {
+        Ninja ninja1=new Ninja("Yassine");
+        System.out.println(ninja1.getName());
+        ninja1.setName("Helmi");
+        System.out.println(ninja1.getName());
+        Ninja ninja2=new Ninja("Aziz");
+     System.out.println(ninja1.displayInfos());   
+        System.out.println("*************");
+       System.out.println(ninja2.displayInfos()); 
+        ninja1.attack(ninja2);
+        System.out.println(ninja1.displayInfos());   
+        System.out.println("*************");
+       System.out.println(ninja2.displayInfos()); 
+ ninja1.attack(ninja2);
+        System.out.println(ninja1.displayInfos());   
+        System.out.println("*************");
+       System.out.println(ninja2.displayInfos());
+       ninja1.restart();
+         ninja2.restart();
+       System.out.println("*************");
+       System.out.println(ninja1.displayInfos());  
+       System.out.println("*************");
+       System.out.println(ninja2.displayInfos());  
+       Ninja.staticDemo("static test","test");
+        System.out.println("*************"); System.out.println("*************"); System.out.println("*************");
+       ninja1.staticDemo("esta","lafiesta");
+       
+    }
+}
